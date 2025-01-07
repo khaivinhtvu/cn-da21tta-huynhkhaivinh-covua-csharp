c9182c9811034847805b715ab60bac762bdd1690
Khải Vinh <kvinh1654377@gmail.com>
2025-01-07 16:22:06 +0700

    Update files

--- Assets/Scripts/GameAiScript.cs
+++ Assets/Scripts/GameAiScript.cs
@@ -14,6 +14,8 @@ public class GameAiScript : MonoBehaviour
     private int[] location = new int[2];
     private MoveScan.MoveInfo moveInfo = new MoveScan.MoveInfo();
     private float[,] aattack;
+    private bool timer = false;
+    private float waittime = 0;
     public delegate void AiMoveInfo(string pieceId, float[,] move);
     public static event AiMoveInfo ShowAiMoveInfo;
 
@@ -35,6 +37,11 @@ public class GameAiScript : MonoBehaviour
 
     private void RaiseMoveInfo()
     {
+        float time = 0;
+        while(time < 10)
+        {
+            time = time + 1;
+        }
         BroadMatrix();
         BFS.BFSALg bfs = new BFS.BFSALg();
         bfs.Matran = this.matran;
@@ -42,16 +49,41 @@ public class GameAiScript : MonoBehaviour
         ShowAiMoveInfo?.Invoke(moveInfo.PieceId, moveInfo.Moves);
     }
 
+    private void TimerBegin()
+    {
+        timer = true;
+    }
+
     private void Start()
     {
         if (active == true)
         {
-            GameScript.CallAiMove += RaiseMoveInfo;
+            GameScript.CallAiMove += TimerBegin;
+        }
+    }
+
+    private void Update()
+    {
+        if (active == true)
+        {
+            if (timer == true)
+            {
+                if ( waittime < 1)
+                {
+                    waittime += Time.deltaTime;
+                }
+                else
+                {
+                    RaiseMoveInfo();
+                    timer = false;
+                    waittime = 0;
+                }
+            }
         }
     }
 
     private void OnDestroy()
     {
-        GameScript.CallAiMove -= RaiseMoveInfo;
+        GameScript.CallAiMove -= TimerBegin;
     }
 }
